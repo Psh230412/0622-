a4dc4ba4b1fb41d98d04b16f1b895e779b90cfb0
GGG <GGG@DESKTOP-8N3GG4O>
2023-06-22 14:41:07 +0900

    나머지

--- 0622 share/src/Math.java
+++ 0622 share/src/Math.java
@@ -1,5 +1,14 @@
 
 public class Math {
+	public static void main(String[] args) {
+		int dividend = 17; 
+		int divisor = 5; 
+
+		int remainder = dividend % divisor; //김민아 나머지 계산
+
+		System.out.println("나머지: " + remainder);
+
+	}
 	// 박상현 더하기  인트값 2개 합 반환
 	
 	// 명완 빼기
